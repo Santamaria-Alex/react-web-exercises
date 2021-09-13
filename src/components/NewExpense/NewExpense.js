40ca5422736103b3904d3e2ff9ede2d9ce328190
Alex Santamaria <santamariaalex146@gmail.com>
2021-09-12 20:42:32 -0500

    rendered the add new expense form conditionally

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,8 +1,11 @@
 
 import './NewExpense.css'
 import ExpenseForm from "./ExpenseForm";
+import React, {useState} from "react";
 
 const NewExpense = (props) => {
+
+    const [isEditing, setIsEditing] = useState(false);
 
     //the parameter (enteredExpenseData) comes from ExpenseForm.js (expenseData)
     const saveExpenseDataHandler = (enteredExpenseData) => {
@@ -11,12 +14,31 @@ const NewExpense = (props) => {
             id: Math.random().toString()
         };
         props.onAddExpense(expenseData);
+        //set to false to go back to default state
+        setIsEditing(false);
+    };
+
+    const startEditingHandler = () => {
+        setIsEditing(true);
+    }
+
+    //this is the functionality for the cancel button, which is in ExpenseForm.js
+    const stopEditingHandler = () => {
+        setIsEditing(false);
     }
 
     return (
-    <div className="new-expense">
-        <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
-    </div>
+        <div className="new-expense">
+            {!isEditing && (
+                <button onClick={startEditingHandler}>Add New Expense</button>)}
+
+            {isEditing && (
+                <ExpenseForm
+                    onSaveExpenseData={saveExpenseDataHandler}
+                    //onCancel is a prop that is being called in the ExpenseForm.js
+                    onCancel={stopEditingHandler}
+                />)}
+        </div>
     )
 }
 
